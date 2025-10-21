27ad9da904a741e80fa6394f71c0fd9d41402f62
Leandro José (Léo) <leandro2203jose@gmail.com>
2025-10-21 05:52:37 +0000

    Blz prosseguir agora

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -951,6 +951,23 @@ export default function Home() {
             </div>
         </div>
 
+        <div className="rn-footer-area rn-section-gap section-separator">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="footer-area text-center">
+                            <div className="logo">
+                                <a href="index.html">
+                                  <Image src={placeholderImages.logo.src} alt="logo" width={120} height={40} data-ai-hint={placeholderImages.logo['data-ai-hint']} />
+                                </a>
+                            </div>
+                            <p className="description mt--30">© 2025. Direitos reservados a <a target="_blank" href="https://github.com/LeBronTech">Lebron Tech.</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
       </main>
     </>
   );
